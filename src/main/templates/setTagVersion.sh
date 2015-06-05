7bad8886e1ce19b6b21579504736b1cda5c99d72
Santiago Aucejo <santiago.aucejo.petzoldt.git@sos-berlin.com>
2015-06-05 15:38:14 +0200

    release build scripts updated with error handling

--- src/main/templates/setTagVersion.sh
+++ src/main/templates/setTagVersion.sh
@@ -6,4 +6,9 @@
 #####################################################################
 echo ********************SET VERSION TO ${releaseVersion}********************
 mvn org.codehaus.mojo:versions-maven-plugin:1.3.1:set -DnewVersion=${releaseVersion} -DgenerateBackupPoms=false
+cc=$?
+if [ $cc -ne 0 ]; then
+    echo "setting new Release Version failed with exit code $cc"
+    exit $cc
+fi
 echo ********************SET VERSION FINISHED********************************
